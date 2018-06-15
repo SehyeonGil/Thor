2530bebe30cd9a2cf53ac9de7aa0bbf2d643850b
KimKiYeong <walkst26@gmail.com>
2018-06-15 10:49:46 +0900

    I LOVE SEHYEON

---
+++ config/passport_KKY.js
@@ -0,0 +1,114 @@
+var express = require('express');
+var app=express();
+var LocalStrategy = require('passport-local').Strategy;
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var KakaoStrategy = require('passport-kakao').Strategy;
+var NaverStrategy = require('passport-naver').Strategy;
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+var Member = require('../models/member');
+var cert = require('../models/certificate');
+var moment = require('moment');
+var bcrypt = require('crypto-browserify');
+var randomstring = require("randomstring");
+var config=require('./passport_config.json');
+module.exports = function(passport,nev) {
+    passport.serializeUser(function (user, done) {
+        done(null, {email: user.email, seller: user.sellercheck, mid: user._id});
+    });
+    passport.deserializeUser(function (user, done) {
+        done(null, user);
+    });
+
+    passport.use('signup', new LocalStrategy({
+            usernameField: 'email',
+            passwordField: 'password',
+            passReqToCallback: true
+        },
+        function (req, email, password, done) {
+            Member.findOne({email: email}, function (err, member) {
+                if (err) return done(err);
+                if (member) {
+                    return done(null, false, {error: '존재하는 이메일입니다.'});
+                } else {
+                    var user = new Member();
+                    user.firstname = req.body.firstname;
+                    user.lastname = req.body.lastname;
+                    user.email = email;
+                    user.provider = 'local';
+                    user.pw = user.generateHash(password);
+                    var usercheck = new cert();
+                    usercheck.email = email;
+                    var key = randomstring.generate(10);
+                    var cipher = bcrypt.createCipher('aes192', key);    // Cipher 객체 생성
+                    cipher.update(email, 'utf8', 'hex');             // 인코딩 방식에 따라 암호화
+                    usercheck.token = cipher.final('hex');
+                    usercheck.timer = moment.duration().add(10,'m').format();
+                    user.save(function (err) {
+                        if (err)
+                            throw err;
+                    });
+                    usercheck.save(function (err) {
+                        if (err)
+                            throw err;
+                        mailer(email, usercheck.token, req.body.firstname);
+                        return done(null, false, {error: 'clear'});
+                        // return done(null, user);
+                    });
+                }
+            });
+        }
+    ));
+
+    passport.use('login', new LocalStrategy({
+            usernameField: 'email',
+            passwordField: 'password',
+            session: true, // 세션에 저장 여부
+            passReqToCallback: true
+        },
+        function (req, email, password, done) {
+            Member.findOne({email: email}, function (err, user) {
+                if (err)
+                    return done(err);
+                if (!user)
+                    return done(null, false, {error: '이메일 에러'});
+                if (!(user.provider === 'local'))
+                    return done(null, false, {error: '타사연동으로 가입된 회원입니다. 위 버튼을 이용해서 로그인해주세요'});
+                if (!(user.validPassword(password)))
+                    return done(null, false, {error: '패스워드 에러'});
+                if (!user.is_certificate)
+                    return done(null, false, {error: user.email});
+                return done(null, user);
+            });
+        })
+    );
+    passport.use(new KakaoStrategy({
+        clientID : config.kakao.clientID,
+        callbackURL : config.kakao.callbackURL
+      },
+      function(accessToken, refreshToken, profile, done){
+        Member.findOne({'naver.id': profile.id}, function (err, user) {
+            if (err)
+                return done(err);
+            else if (!user){
+                
+            }
+            else{
+                return done(null, user);
+            }
+        });
+          console.log(profile);
+      }
+    ));
+    passport.use(new NaverStrategy({
+        clientID:config.naver.clientID,
+        clientSecret:config.naver.clientSecret,
+        callbackURL:config.naver.callbackURL
+    },
+        function(accessToken,refreshToken,profile,done){
+            console.log(profile);
+        }
+    ));
+        
+};
---
+++ routes/member/index_KKY.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Sehyeon on 2018-06-03.
+ */
+var express = require('express');
+var router = express.Router();
+var controller=require('./memberController');
+
+/* GET users listing. */
+router.get('/Mail_Confirm_Complete/:id',controller.mailConfirmComplete);
+
+/*router.get('/Join',controller.join);
+router.get('/Join_Confirm',controller.joinConfirm);
+router.get('/Login',controller.login);
+router.get('/Mail_Confirm',controller.mailConfirm);
+router.get('/Find_Password',controller.findPassword);
+router.get('/Identify_Certificate',controller.idnetifyCertificate);
+router.get('/Manage',controller.manage);
+router.get('/Address_Delivery',controller.addressDelivery);
+router.get('/Mypage',controller.myPage);
+router.get('/Manage_Order',controller.manageOrder);
+router.get('/Manage_Review',controller.manageReview);
+router.get('/Favorite',controller.favorite);
+router.get('/Payment_List',controller.paymentList);
+router.get('/Message',controller.message);*/
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
++/*
++router.get('/GoogleLogin',controller.googleLogin);
++router.get('/GoogleLoginCallback',controller.googleLoginCallback);
++*/
+router.get('/KakaoLogin',controller.kakaoLogin);
+router.get('/KakaoLoginCallback',controller.kakaoLoginCallback);
+router.get('/NaverLogin',controller.naverLogin);
+router.get('/NaverLoginCallback',controller.naverLoginCallback);
+
+router.post('/SignUp',controller.normalSignup);
+router.post('/Login', controller.loginAttemp);
+router.post('/reconfirm',controller.reconfirm);
+
+module.exports = router;
