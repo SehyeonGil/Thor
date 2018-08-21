360bfcf39d660186b02cb38de84c85f45d3a60ec
abab0528 <abab0528@naver.com>
2018-08-21 16:13:03 +0900

    register update

--- public/js/custom.js
+++ public/js/custom.js
@@ -103,17 +103,7 @@
         // } 
 
 
-        $("#moveBtn").on("click",function(event){
-            // 이동 버튼을 클릭시 pre 태그로 스크롤의 위치가 이동되도록 한다.
 
-            // 1. pre태그의 위치를 가지고 있는 객체를 얻어온다. => offset 객체
-            var offset = $("#scroll").offset();
-
-            // offset은 절대 위치를 가져온다. offset.top을 통해 상단의 좌표를 가져온다.
-            // position은 부모를 기준으로한 상대위치를 가져온다.
-            $("html body").animate({scrollTop:offset.top},2000);
-
-        });
 
 
         // -----------------------------
